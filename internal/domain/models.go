4b514e947d2da2cbe3f2d72dd146eadc59151dd9
dtrober <dylantrobertson1445@gmail.com>
2025-03-17 23:45:38 -0500

    Starting work on Ingest form. Need to swap to place intergration

--- internal/domain/models.go
+++ internal/domain/models.go
@@ -31,3 +31,19 @@ type UserResponse struct {
 type ErrorResponse struct {
 	Error string `json:"error"`
 }
+
+// Lead represents a sales lead
+type Lead struct {
+	ID            int       `json:"id"`
+	CompanyName   string    `json:"company_name"`
+	PhoneNumber   string    `json:"phone_number"`
+	Address       string    `json:"address"`
+	Email         string    `json:"email"`
+	Website       string    `json:"website"`
+	WebsiteScore  int       `json:"website_score"`
+	PreRenderSite bool      `json:"pre_render_site"`
+	ReviewAvg     int       `json:"review_avg"`
+	ReviewDate    time.Time `json:"review_date"`
+	CreatedAt     time.Time `json:"created_at"`
+	UpdatedAt     time.Time `json:"updated_at"`
+}
